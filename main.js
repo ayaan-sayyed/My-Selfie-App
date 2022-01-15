f7e125f78dcbe93f71861d6a09bd92493363ce11
ayaan-sayyed <84319719+ayaan-sayyed@users.noreply.github.com>
2022-01-15 12:02:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var SpeechRecognition= window.webkitSpeechRecognition;
+
+var Recognition = new SpeechRecognition();
+
+function Start() {
+document.getElementById("textbox").innerHTML="";
+Recognition.start();
+}
+
+Recognition.onresult= function(event) {
+console.log(event);
+
+var Content= event.results[0] [0].transcript;
+console.log(Content);
+
+document.getElementById("textbox").innerHTML= Content;
+}
