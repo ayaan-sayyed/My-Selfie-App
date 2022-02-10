93efbce2995491559e6a8205067f205d9191abf8
ayaan-sayyed <84319719+ayaan-sayyed@users.noreply.github.com>
2022-02-10 18:01:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,17 +12,26 @@ console.log(event);
 
 var Content= event.results[0] [0].transcript;
 console.log(Content);
-
 document.getElementById("textbox").innerHTML= Content;
-speak();
+if(Content== "take my selfie")
+{
+    console.log("taking your selfie !!!!!!!!!!!!!!!!!!");
+    speak();
+}
 }
 
 function speak() {
 var synth= window.speechSynthesis;
-speak_data= document.getElementById("textbox").value;
+speak_data="Taking your selfie in 5 seconds";
 var utterThis= new SpeechSynthesisUtterance(speak_data);
 synth.speak(utterThis);
 Webcam.attach(camera);
+setTimeout(function()
+{
+    take_snapshot();
+    save();
+
+},5000);
 }
 
 Webcam.set({
@@ -33,3 +42,19 @@ jpeg_quality: 90
 });
 
 camera= document.getElementById("camera");
+
+
+function take_snapshot() 
+{
+
+Webcam.snap(function(data_uri) {
+document.getElementById("result").innerHTML= '<img id="selfie_image" src="'+data_uri+'">';
+});
+}
+
+function save() {
+link= document.getElementById("link");
+image= document.getElementById("selfie_image").src ;
+link.href= image;
+link.click();
+}
